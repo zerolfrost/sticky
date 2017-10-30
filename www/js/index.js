67afe76c55ac9c5f77ce8fb44238d6de4641ee44
zeroLfrost <germachine33@gmail.com>
2017-10-30 16:25:16 +0800

    initial update

---
+++ www/js/index.js
@@ -0,0 +1,6 @@
+function In(){
+    window.localStorage['write']=document.getElementById("sulat").value
+    }
+function Show(){
+    document.getElementById("sulat").value=window.localStorage['write']
+    }
